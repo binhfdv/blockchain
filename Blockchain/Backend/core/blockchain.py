03b034ba9aaf6dc1abc96a24e7510e2563b00c7c
binhfdv <dvanbinh10@gmail.com>
2024-08-10 06:29:51 +0000

    transaction

--- Blockchain/Backend/core/blockchain.py
+++ Blockchain/Backend/core/blockchain.py
@@ -5,6 +5,7 @@ from Blockchain.Backend.core.block import Block
 from Blockchain.Backend.core.blockheader import BlockHeader
 from Blockchain.Backend.util.util import hash256
 from Blockchain.Backend.core.database.database import BlockchainDB
+from Blockchain.Backend.core.Tx import CoinbaseTx
 import time
 
 
@@ -31,12 +32,14 @@ class Blockchain:
 
     def addBlock(self, BlockHeight, prevBlockHash):
         timestamp = int(time.time())
-        Transaction = f"Agent A sent {BlockHeight} BTC to Ojea"
-        merkleRoot = hash256(Transaction.encode()).hex()
+        coinbaseInstance = CoinbaseTx(BlockHeight)
+        coinbaseTx = coinbaseInstance.CoinbaseTransaction()
+
+        merkleRoot = ' '
         bits = 'ffff001f'
         blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
         blockheader.mine()
-        self.write_on_disk([Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__])
+        self.write_on_disk([Block(BlockHeight, 1, blockheader.__dict__, 1, coinbaseTx.to_dict()).__dict__])
 
     def main(self):
         while True:
--- Blockchain/Backend/util/util.py
+++ Blockchain/Backend/util/util.py
@@ -1,8 +1,38 @@
 from Crypto.Hash import RIPEMD160
 from hashlib import sha256
+from math import log
+
+from Blockchain.Backend.core.EllepticCurve.EllepticCurve import BASE58_ALPHABET
 
 def hash256(s):
     return sha256(sha256(s).digest()).digest()
 
 def hash160(s):
     return RIPEMD160.new(sha256(s).digest()).digest()
+
+def bytes_needed(n):
+    if n == 0:
+        return 1
+    return int(log(n, 256)) + 1
+
+def int_to_little_endian(n, length):
+    """Int to little endian, take int and returns the little-endian byte sequence of length"""
+    return n.to_bytes(length, 'little')
+
+def little_endian_to_int(b):
+    return int.from_bytes(b, 'little')
+
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    
+    combined = num.to_bytes(25, byteorder = 'big')
+    checksum = combined[-4:]
+
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError(f'bad address {checksum} {hash256(combined[:-4][:4])}')
+
+
+    return combined[1:-4]
